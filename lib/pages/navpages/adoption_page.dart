76e718806db6bc4663cc4189b617370bdca87947
julianakemi <julianakemide@gmail.com>
2023-10-15 12:27:40 -0400

    implements adoption page

--- lib/pages/navpages/adoption_page.dart
+++ lib/pages/navpages/adoption_page.dart
@@ -1,14 +1,103 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:paw_finder/cubit/app_cubit_states.dart';
+import 'package:paw_finder/cubit/app_cubits.dart';
+import 'package:paw_finder/misc/colors.dart';
+import 'package:paw_finder/widgets/horizontal_card.dart';
 
 class AdoptionPage extends StatelessWidget {
   const AdoptionPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("Adoption page")
-      ),
-    );
+    return Scaffold(
+        body: BlocBuilder<AppCubits, CubitStates>(builder: (context, state) {
+      if (state is LoadedState) {
+        var info = state.pets;
+        return Column(
+          children: [
+            Stack(children: [
+              Container(
+                    width: double.infinity,
+                    height: MediaQuery.of(context).size.height / 3.3,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(
+                            'img/background-blob.png'), // Replace with your image
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                    child: SafeArea(
+                      child: Padding(
+                        padding: const EdgeInsets.only(top:36.0, left: 14, right: 14),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Adoção responsável",
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white)),
+                            SizedBox(height: 10),
+                            SearchAnchor(builder: (BuildContext context,
+                                SearchController controller) {
+                              return SearchBar(
+                                hintText: "Busque",
+                                controller: controller,
+                                padding: const MaterialStatePropertyAll<EdgeInsets>(
+                                    EdgeInsets.symmetric(horizontal: 16.0)),
+                                onTap: () {
+                                  controller.openView();
+                                },
+                                onChanged: (_) {
+                                  controller.openView();
+                                },
+                                leading: const Icon(Icons.search),
+                                trailing: <Widget>[
+                                  Tooltip(
+                                    message: 'Filtros',
+                                    child: IconButton(
+                                      onPressed: () {},
+                                      icon: const Icon(Icons.filter_list),
+                                      selectedIcon:
+                                          const Icon(Icons.brightness_2_outlined),
+                                    ),
+                                  )
+                                ],
+                              );
+                            }, suggestionsBuilder: (BuildContext context,
+                                SearchController controller) {
+                              return List<ListTile>.generate(5, (int index) {
+                                final String item = 'item $index';
+                                return ListTile(
+                                  title: Text(item),
+                                  onTap: () {},
+                                );
+                              });
+                            }),
+                          ]),
+                      ),
+                    )
+                  ),
+            ]),
+            Expanded(
+              child: ListView.builder(
+                scrollDirection: Axis.vertical,
+                itemCount: info.length,
+                itemBuilder: (context, index) {
+                  return HorizontalCard(
+                      location: info[index].location,
+                      date: info[index].color,
+                      breed: info[index].breed,
+                      gender: info[index].gender);
+                },
+              ),
+            ),
+          ],
+        );
+      } else {
+        return Container();
+      }
+    }));
   }
 }
--- lib/pages/navpages/search_page.dart
+++ lib/pages/navpages/search_page.dart
@@ -16,68 +16,80 @@ class SearchPage extends StatelessWidget {
         var info = state.pets;
         return Column(
           children: [
-            Container(
-                height: 240,
-                alignment: Alignment.centerLeft,
-                width: double.infinity,
-                padding: const EdgeInsets.only(top: 80, left: 20, right: 20),
-                decoration: BoxDecoration(color: AppColors.primary400),
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("Busque seu pet",
-                          style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white)),
-                      SizedBox(height: 20),
-                      SearchAnchor(builder:
-                          (BuildContext context, SearchController controller) {
-                        return SearchBar(
-                          hintText: "Busque por bairro",
-                          controller: controller,
-                          padding: const MaterialStatePropertyAll<EdgeInsets>(
-                              EdgeInsets.symmetric(horizontal: 16.0)),
-                          onTap: () {
-                            controller.openView();
-                          },
-                          onChanged: (_) {
-                            controller.openView();
-                          },
-                          leading: const Icon(Icons.search),
-                          trailing: <Widget>[
-                            Tooltip(
-                              message: 'Filtros',
-                              child: IconButton(
-                                onPressed: () {},
-                                icon: const Icon(Icons.filter_list),
-                                selectedIcon:
-                                    const Icon(Icons.brightness_2_outlined),
-                              ),
-                            )
-                          ],
-                        );
-                      }, suggestionsBuilder:
-                          (BuildContext context, SearchController controller) {
-                        return List<ListTile>.generate(5, (int index) {
-                          final String item = 'item $index';
-                          return ListTile(
-                            title: Text(item),
-                            onTap: () {},
-                          );
-                        });
-                      }),
-                    ])),
+            Stack(children: [
+              Container(
+                    width: double.infinity,
+                    height: MediaQuery.of(context).size.height / 3.3,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(
+                            'img/background-blob.png'), // Replace with your image
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                    child: SafeArea(
+                      child: Padding(
+                        padding: const EdgeInsets.only(top:36.0, left: 14, right: 14),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Busque seu pet",
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white)),
+                            SizedBox(height: 10),
+                            SearchAnchor(builder: (BuildContext context,
+                                SearchController controller) {
+                              return SearchBar(
+                                hintText: "Busque por bairro",
+                                controller: controller,
+                                padding: const MaterialStatePropertyAll<EdgeInsets>(
+                                    EdgeInsets.symmetric(horizontal: 16.0)),
+                                onTap: () {
+                                  controller.openView();
+                                },
+                                onChanged: (_) {
+                                  controller.openView();
+                                },
+                                leading: const Icon(Icons.search),
+                                trailing: <Widget>[
+                                  Tooltip(
+                                    message: 'Filtros',
+                                    child: IconButton(
+                                      onPressed: () {},
+                                      icon: const Icon(Icons.filter_list),
+                                      selectedIcon:
+                                          const Icon(Icons.brightness_2_outlined),
+                                    ),
+                                  )
+                                ],
+                              );
+                            }, suggestionsBuilder: (BuildContext context,
+                                SearchController controller) {
+                              return List<ListTile>.generate(5, (int index) {
+                                final String item = 'item $index';
+                                return ListTile(
+                                  title: Text(item),
+                                  onTap: () {},
+                                );
+                              });
+                            }),
+                          ]),
+                      ),
+                    )
+                  ),
+            ]),
             Expanded(
               child: ListView.builder(
                 scrollDirection: Axis.vertical,
                 itemCount: info.length,
                 itemBuilder: (context, index) {
                   return HorizontalCard(
-                      location: info[index].location, 
+                      location: info[index].location,
                       date: info[index].color,
                       breed: info[index].breed,
-                      gender:info[index].gender);
+                      gender: info[index].gender);
                 },
               ),
             ),
